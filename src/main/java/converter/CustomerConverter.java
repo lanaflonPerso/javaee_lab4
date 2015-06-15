0d2d5841c22285f91da9e05e24cf03963fe3e046
Vadym <vad.pechenoga@gmail.com>
2015-06-16 00:49:02 +0300

    Add Customer Converter

---
+++ src/main/java/converter/CustomerConverter.java
@@ -0,0 +1,32 @@
+package converter;
+
+import bean.DAO;
+import model.Customer;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(forClass=Customer.class)
+public class CustomerConverter implements Converter {
+
+    public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+        if (value == null || value.length() == 0) {
+            return null;
+        }
+        return DAO.getById(Integer.parseInt(value), Customer.class);
+    }
+
+    public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+        if (object == null) {
+            return null;
+        }
+        if (object instanceof Customer) {
+            Customer o = (Customer) object;
+            return o.getId() + "";
+        } else {
+            throw new IllegalArgumentException("unexpected type: "+object.getClass().getName());
+        }
+    }
+}
