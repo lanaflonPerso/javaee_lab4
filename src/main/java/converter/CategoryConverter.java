8726ff3ac701dd3c0fa0f2cc533dcd62eb3383a5
Vadym <vad.pechenoga@gmail.com>
2015-06-15 20:58:21 +0300

    Add Category converter

---
+++ src/main/java/converter/CategoryConverter.java
@@ -0,0 +1,32 @@
+package converter;
+
+import bean.DAO;
+import model.Category;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(forClass=Category.class)
+public class CategoryConverter implements Converter {
+
+    public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+        if (value == null || value.length() == 0) {
+            return null;
+        }
+        return DAO.getById(Integer.valueOf(value), Category.class);
+    }
+
+    public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+        if (object == null) {
+            return null;
+        }
+        if (object instanceof Category) {
+            Category o = (Category) object;
+            return o.getId() + "";
+        } else {
+            throw new IllegalArgumentException("unexpected type: "+object.getClass().getName());
+        }
+    }
+}
